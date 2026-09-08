8688a7ef1366e16cef67ca5ab5a033a9edbc7871
valerieeys <anindita1227204@gmail.com>
2026-09-08 21:48:42 +0700

    biasalah

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,6 +2,3 @@ export const SUPABASE_CONFIG = {
   url: import.meta.env.VITE_SUPABASE_URL,
   anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
 };
-  url: "https://zhbidygtahzszhiqpwjh.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoYmlkeWd0YWh6c3poaXFwd2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NTg4MjEsImV4cCI6MjEwNDQzNDgyMX0.YpWhatCPIZL2fKfNTGC6o94LvxU1-P02gLtaF5qXRfg"
-};
